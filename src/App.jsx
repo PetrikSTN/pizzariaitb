f44ec9f63f3ba739d557f1fd3be994a6cf2f8dd9
PetrikSTN <henriquepetrik07@gmail.com>
2024-08-07 10:49:51 -0300

    1commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import {useState} from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import Listagem from './pages/Listagem';
 
 const App = () => {
 
-  const [color] = useState("#0000FF")
-  document.body.style.backgroundColor = color
-
-  return (
+  return(
     <Routes>
-      <Route path="/" element={<Listagem />}/>
+      <Route path="/" />
     </Routes>
   )
 }
